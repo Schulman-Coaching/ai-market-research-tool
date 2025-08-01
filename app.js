d741ef02d83a492d52323da5401d74912a08db6f
elieschulman <elie@cohortlearninglabs.org>
2025-08-01 06:53:32 -0400

    Try simple 'sonar' model name for Perplexity API

--- app.js
+++ app.js
@@ -362,7 +362,7 @@ Focus on creating a practical, implementable solution that could be developed as
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
-                    model: 'llama-3.1-sonar-small-128k-online',
+                    model: 'sonar',
                     messages: [
                         {
                             role: 'user',
